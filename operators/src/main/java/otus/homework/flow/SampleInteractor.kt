0a4306f0e9fd213db2b91db762784a5f3fb1ff2d
OlgaRiabkova <oliabric@gmail.com>
2022-04-28 16:11:40 +0500

    2

--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -18,7 +18,16 @@ class SampleInteractor(
      * 6) возвращает результат
      */
     fun task1(): Flow<String> {
-        return flowOf()
+        return flowOf(7, 12, 4, 8, 11, 5, 7, 16, 99, 1)
+//            .map { value -> value.toDouble().pow(5).toInt() }
+            .filter { it <= 20 }
+            .filter { it % 2 != 0 }
+            .map { value -> "$value won" }
+            .take(3)
+
+        // судя по ожидаемому результату, должно быть не возведение в 5ю степень, а умножение на 5
+        // и еще непонятно, как убирать числа <=20, если они в 5й степени или умножены на 5
+        // тогда правильный ответ должен быть "1 won"
     }
 
     /**
@@ -28,8 +37,26 @@ class SampleInteractor(
      * Если входное число делится на 15 - эмитим само число и после него эмитим строку FizzBuzz
      * Если число не делится на 3,5,15 - эмитим само число
      */
-    fun task2(): Flow<String> {
-        return flowOf()
+    fun task2(): Flow<String> = flow {
+        for (i in 1..21) {
+            when {
+                i % 15 == 0 -> {
+                    emit(i.toString())
+                    emit("FizzBuzz")
+                }
+                i % 3 == 0 -> {
+                    emit(i.toString())
+                    emit("Fizz")
+                }
+                i % 5 == 0 -> {
+                    emit(i.toString())
+                    emit("Buzz")
+                }
+                else -> {
+                    emit(i.toString())
+                }
+            }
+        }
     }
 
     /**
@@ -38,7 +65,15 @@ class SampleInteractor(
      * Если айтемы в одно из флоу кончились то результирующий флоу также должен закончится
      */
     fun task3(): Flow<Pair<String, String>> {
-        return flowOf()
+        val flow1 = flowOf(
+            "Red",
+            "Green",
+            "Blue",
+            "Black",
+            "White"
+        )
+        val flow2 = flowOf("Circle", "Square", "Triangle")
+        return flow1.zip(flow2) { a, b -> Pair(a,b) }
     }
 
     /**
@@ -48,6 +83,18 @@ class SampleInteractor(
      * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
      */
     fun task4(): Flow<Int> {
-        return flowOf()
+        return flow {
+            (1..10).forEach {
+                if (it == 5) {
+                    throw IllegalArgumentException("Failed")
+                } else {
+                    emit(it)
+                }
+            }
+        }.catch {
+            emit(-1)
+        }
     }
+
+    // непонятно, что нужно сделать для dotsRepository.completed()
 }
