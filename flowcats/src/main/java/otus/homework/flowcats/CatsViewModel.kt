6a1fad2799d924fb89bab5d450de83489c537688
OlgaRiabkova <oliabric@gmail.com>
2022-04-28 12:21:05 +0500

    1

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -1,26 +1,38 @@
 package otus.homework.flowcats
 
-import androidx.lifecycle.*
-import kotlinx.coroutines.Dispatchers
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import otus.homework.flowcats.CatsViewModel.Result.Initial
 
 class CatsViewModel(
     private val catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
+    private val _uiState = MutableStateFlow<Result>(Initial)
+    val uiState: StateFlow<Result> = _uiState
 
     init {
         viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.value = it
+            catsRepository.listenForCatFacts()
+                .catch {
+                    _uiState.value = Result.Error("Ошибка выполнения запроса")
                 }
-            }
+                .collect {
+                    _uiState.value = Result.Success(it)
+                }
         }
+    }
+
+    sealed class Result {
+        object Initial : Result()
+        class Success(val catData: Fact) : Result()
+        class Error(val message: String) : Result()
     }
 }
 
